efa3c90d14021b6a4810411b22043313fee13ba4
Hubert Droździak <hubertdrozdziak@gmail.com>
2023-09-28 21:07:15 +0200

    refactor tests

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,12 +1,18 @@
 #include <gtest/gtest.h>
 #include "../lib/Specimen.hpp"
 
-TEST(SpecimenClass, ConstructorAndGetPath){
-    EXPECT_FALSE(std::is_default_constructible<Specimen>::value);
+struct SpecimenClass : public ::testing::Test{
     Specimen s = Specimen(4, 2);
+};
+
+TEST(SpecimenConstructor, NoDefaultConstructor){
+    EXPECT_FALSE(std::is_default_constructible<Specimen>::value);
+}
+
+TEST_F(SpecimenClass, GetPath){
     int pathLength = static_cast<int>(s.getPath().size());
     EXPECT_EQ(pathLength, 5);
-    EXPECT_EQ(s.getPath()[0], 2);
-    EXPECT_EQ(s.getPath()[pathLength-1], 2);
+    EXPECT_EQ(s.getPath().front(), 2);
+    EXPECT_EQ(s.getPath().back(), 2);
     EXPECT_NE(s.getPath(), std::vector<int>({2,0,1,3,2}));
 }
