6589f6f8202e60f1f9975ca33f5257732c96c129
Hubert Droździak <hubertdrozdziak@gmail.com>
2023-09-28 20:53:02 +0200

    a little refactor

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,14 +1,6 @@
 #include <gtest/gtest.h>
 #include "../lib/Specimen.hpp"
 
-namespace{
-    int GetMeaningOfLife() {return 42;}
-}
-
-TEST(LifeMeaning, getMeaningOfLife){
-    ASSERT_EQ(GetMeaningOfLife(), 42);
-}
-
 TEST(SpecimenClass, ConstructorAndGetPath){
     EXPECT_FALSE(std::is_default_constructible<Specimen>::value);
     Specimen s = Specimen(4, 2);
